916702868abca4f56d0d5581d90038cc055fc5b8
1 <hwon466@gmail.com>
2023-01-24 19:11:32 +0900

    20230124

--- 점의 위치 구하기.js
+++ 점의 위치 구하기.js
@@ -9,3 +9,29 @@ function solution(dot) {
     return 4;
   }
 }
+
+
+
+function solution(dot) {
+    var answer = 0;
+    const x = dot[0];
+    const y = dot[1];
+
+    if (x > 0 && y > 0) answer = 1
+    if (x < 0 && y > 0) answer = 2
+    if (x < 0 && y < 0) answer = 3
+    if (x > 0 && y < 0) answer = 4
+
+    return answer;
+}
+
+
+
+function solution(dot) {
+  var answer = 0;
+  if (dot[0] > 0 && dot[1] > 0) answer = 1;
+  if (dot[0] < 0 && dot[1] > 0) answer = 2;
+  if (dot[0] < 0 && dot[1] < 0) answer = 3;
+  if (dot[0] > 0 && dot[1] < 0) answer = 4;
+  return answer;
+}
