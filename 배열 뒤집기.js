3d832fb624d49c3e61cf07aa8963bcc3c87a8fd6
1 <hwon466@gmail.com>
2023-01-26 02:17:42 +0900

    20230126

---
+++ 배열 뒤집기.js
@@ -0,0 +1,23 @@
+function solution(num_list) {
+  let answer = [];
+  for (let i = num_list.length - 1; i >= 0; i--) {
+    answer.push(num_list[i]);
+  }
+  return answer;
+}
+
+//다른풀이
+function solution(num_list) {
+  var answer = [];
+  var j = num_list.length;
+  for (var i = 1; i <= j; i++) {
+    answer.push(num_list[j - i]);
+  }
+  return answer;
+}
+
+
+
+function solution(num_list) {
+    return num_list.reverse()
+}
