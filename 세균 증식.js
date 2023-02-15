da9123f95da708c623790bf178125bebac517d98
WON <hwon46@naver.com>
2023-02-15 23:56:17 +0900

    20230215

--- 세균 증식.js
+++ 세균 증식.js
@@ -3,3 +3,10 @@ function solution(n, t) {
     answer=Math.pow(2,t)*n
     return answer;
 }
+
+//다른 풀이
+function solution(n, t) {
+    var answer = 0;
+    answer = n * (2**t);
+    return answer;
+}
